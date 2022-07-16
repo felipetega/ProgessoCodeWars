d3f68c78a2d289b49543a28dde98d07c1bb27efd
Felipe Crema Ribeiro <100156699+felipetega@users.noreply.github.com>
2022-07-16 15:23:21 -0300

    quase lá

--- plot.py
+++ plot.py
@@ -26,10 +26,9 @@ font1 = {'family': 'serif', 'color': 'darkred', 'size': 20}
 font2 = {'family': 'serif', 'color': 'black', 'size': 15}
 
 # PLOT 1
-plt.subplot(2, 1, 1)
+plt.subplot(2, 2, 1)
 # STYLE
-plt.title("CodeWars", fontdict=font1)
-plt.xlabel("Date", fontdict=font2)
+plt.title("All Data", fontdict=font1)
 plt.ylabel("logarithmic scale", fontdict=font2)
 plt.grid(axis='both', color='gray', linestyle='-', linewidth=1)
 plt.yscale("log")
@@ -39,4 +38,36 @@ plt.plot(Xdate, Yhonor, c="r")
 plt.plot(Xdate, Ytotal, c="y")
 plt.plot(Xdate, Yranking, c="black")
 
+# PLOT 2
+plt.subplot(2, 2, 2)
+# STYLE
+plt.title("Kyu, Challenges and Honor", fontdict=font1)
+plt.ylabel("normal scale", fontdict=font2)
+plt.grid(axis='both', color='gray', linestyle='-', linewidth=1)
+# DATA
+plt.plot(Xdate, Ykyu, c="b")
+plt.plot(Xdate, Yhonor, c="r")
+plt.plot(Xdate, Ytotal, c="y")
+
+# PLOT 3
+plt.subplot(2, 2, 3)
+# STYLE
+plt.title("Ranking Position", fontdict=font1)
+plt.xlabel("Date", fontdict=font2)
+plt.ylabel("normal scale", fontdict=font2)
+plt.grid(axis='both', color='gray', linestyle='-', linewidth=1)
+# DATA
+plt.plot(Xdate, Yranking, c="black")
+
+# PLOT 4
+plt.subplot(2, 2, 4)
+# STYLE
+plt.title("Completed Challenges", fontdict=font1)
+plt.xlabel("Date", fontdict=font2)
+plt.ylabel("normal scale", fontdict=font2)
+plt.grid(axis='both', color='gray', linestyle='-', linewidth=1)
+# DATA
+plt.plot(Xdate, Ytotal, c="y")
+
+# SHOW
 plt.show()
