67940bffa77bf5c5f919ac90327fa0051d3e12b6
Felipe Crema Ribeiro <100156699+felipetega@users.noreply.github.com>
2022-07-16 14:59:50 -0300

    enfeitando

--- plot.py
+++ plot.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# DATASET
 x = pd.read_excel("Stats.xlsx")
 
 Xdate = []
@@ -10,19 +11,32 @@ Ytotal = []
 Yranking = []
 
 for i in x["Date"]:
-     Xdate.append(i)
+    Xdate.append(i)
 for i in x["Kyu"]:
     Ykyu.append(i)
 for i in x["Honor"]:
-  Yhonor.append(i)
+    Yhonor.append(i)
 for i in x["Total Completed"]:
-  Ytotal.append(i)
+    Ytotal.append(i)
 for i in x["Ranking"]:
-  Yranking.append(i)
+    Yranking.append(i)
 
+# FONTS
+font1 = {'family': 'serif', 'color': 'darkred', 'size': 20}
+font2 = {'family': 'serif', 'color': 'black', 'size': 15}
+
+# PLOT 1
+plt.subplot(2, 1, 1)
+# STYLE
+plt.title("CodeWars", fontdict=font1)
+plt.xlabel("Date", fontdict=font2)
+plt.ylabel("logarithmic scale", fontdict=font2)
+plt.grid(axis='both', color='gray', linestyle='-', linewidth=1)
 plt.yscale("log")
+# DATA
 plt.plot(Xdate, Ykyu, c="b")
 plt.plot(Xdate, Yhonor, c="r")
 plt.plot(Xdate, Ytotal, c="y")
-#plt.plot(Xdate, Yranking, c="black")
+plt.plot(Xdate, Yranking, c="black")
+
 plt.show()
